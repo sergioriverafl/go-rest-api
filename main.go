fffb042bc9b30a7d4480c148a6b6bc4dbac8f4dc
Sergio Rivera <sergioriverafl@gmail.com>
2024-01-28 14:12:04 -0500

    (Init) Add router and http server

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type task struct {
+	ID      int    `json:ID`
+	Name    string `json:Name`
+	Content string `json:Content`
+}
+
+type allTasks []task
+
+var tasks = allTasks{
+	{
+		ID:      1,
+		Name:    "Coding run quote",
+		Content: "Connect question services witch form model in frontend interface",
+	},
+	{
+		ID:      2,
+		Name:    "Organize git hub personal repositories",
+		Content: "Some content",
+	},
+}
+
+func indexRoute(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to api")
+}
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", indexRoute)
+	log.Fatal(http.ListenAndServe(":20000", router))
+}
