07840be4070e053be0afd880ccab5d2ac8ff8e00
Sergio Rivera <sergioriverafl@gmail.com>
2024-01-28 23:47:20 -0500

    (fix) rafactor project

--- main.go
+++ main.go
@@ -1,135 +1,27 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"strconv"
 
 	"github.com/gorilla/mux"
+	"github.com/sergioriverafl/go-rest-api/handlers"
 )
 
-type task struct {
-	ID      int    `json:ID`
-	Name    string `json:Name`
-	Content string `json:Content`
-}
-
-type allTasks []task
-
-var tasks = allTasks{
-	{
-		ID:      1,
-		Name:    "Coding run quote",
-		Content: "Connect question services witch form model in frontend interface",
-	},
-	{
-		ID:      2,
-		Name:    "Organize git hub personal repositories",
-		Content: "Some content",
-	},
-}
-
 func indexRoute(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to api v1")
-}
-
-func createTask(w http.ResponseWriter, r *http.Request) {
-	var newTask task
-	reqBody, err := ioutil.ReadAll(r.Body)
-
-	if err != nil {
-		fmt.Fprintf(w, "Insert a valid task")
-	}
-
-	json.Unmarshal(reqBody, &newTask)
-
-	newTask.ID = len(tasks) + 1
-	tasks = append(tasks, newTask)
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(newTask)
-}
-
-func getTasks(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(tasks)
-}
-
-func getTask(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	taskID, err := strconv.Atoi(vars["id"])
-
-	if err != nil {
-		fmt.Fprintf(w, "Invalid ID")
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-
-	for _, task := range tasks {
-		if task.ID == taskID {
-			json.NewEncoder(w).Encode(task)
-		}
-	}
-}
-
-func deleteTask(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	taskID, err := strconv.Atoi(vars["id"])
-
-	if err != nil {
-		fmt.Fprintf(w, "Invalid ID")
-	}
-
-	for i, task := range tasks {
-		if task.ID == taskID {
-			tasks = append(tasks[:i], tasks[i+1:]...)
-			w.WriteHeader(http.StatusAccepted)
-			fmt.Fprintf(w, "The task witch ID %v has been remove successfully", taskID)
-		}
-	}
-}
-
-func updateTask(w http.ResponseWriter, r *http.Request) {
-
-	vars := mux.Vars(r)
-	taskID, err := strconv.Atoi(vars["id"])
-	var updatedTask task
-
-	if err != nil {
-		fmt.Fprintf(w, "Invalid ID")
-	}
-
-	reqBody, err := ioutil.ReadAll(r.Body)
-
-	if err != nil {
-		fmt.Fprintf(w, "Please enter valid data")
-	}
-
-	json.Unmarshal(reqBody, &updatedTask)
-
-	for i, task := range tasks {
-		if task.ID == taskID {
-			tasks = append(tasks[:i], tasks[i+1:]...)
-			updatedTask.ID = taskID
-			tasks = append(tasks, updatedTask)
-
-			fmt.Fprintf(w, "The task witch ID %v has been updated", taskID)
-		}
-	}
-
-}
-
-func main() {
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", indexRoute)
-	router.HandleFunc("/tasks", getTasks).Methods("GET")
-	router.HandleFunc("/tasks", createTask).Methods("POST")
-	router.HandleFunc("/tasks/{id}", getTask).Methods("GET")
-	router.HandleFunc("/tasks/{id}", deleteTask).Methods("DELETE")
-	router.HandleFunc("/tasks/{id}", updateTask).Methods("PUT")
-	log.Fatal(http.ListenAndServe(":65535", router))
+
+	// Index Routes
+	router.HandleFunc("/", handlers.IndexRoute)
+
+	// Tasks Routes
+	router.HandleFunc("/tasks", handlers.CreateTask).Methods("POST")
+	router.HandleFunc("/tasks", handlers.GetTasks).Methods("GET")
+	router.HandleFunc("/tasks/{id}", handlers.GetOneTask).Methods("GET")
+	router.HandleFunc("/tasks/{id}", handlers.DeleteTask).Methods("DELETE")
+	router.HandleFunc("/tasks/{id}", handlers.UpdateTask).Methods("PUT")
+
+	fmt.Println("Server started on port ", 3000)
+	log.Fatal(http.ListenAndServe(":3000", router))
 }
